bb7c0b9bc1583aff5cbf3b6b39211837d6f0e12c
reginleiff <reginleiff@gmail.com>
2018-06-06 14:50:02 +0800

    feat: add buttons for rooms

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 		url: "http://localhost:8080/api/get/rooms"
 	}).then(function(data) {
 		$.each(JSON.parse(data), function(i, room) {
-			var room_elem = ("<li class='square room'>" + room.status + "</li>")
+			var room_elem = ("<button type=`button` class='list-group-item list-group-item-action'> Room " 
+				+ i + "       Status: " + room.status + "</li>")
 			$('#rooms').append(room_elem)	
 		})
 	});
