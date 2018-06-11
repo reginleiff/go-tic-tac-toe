f2c0794c13edffb6233d475309004f8ced45306f
Yeo Junkai <reginleiff@gmail.com>
2018-06-11 18:27:05 +0800

    feat: update test data

--- db/migrations/00002_TestData.sql
+++ db/migrations/00002_TestData.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-INSERT INTO boards (created_at, updated_at) VALUES (NOW(), NOW());
-INSERT INTO boards (created_at, updated_at) VALUES (NOW(), NOW());
+INSERT INTO boards (player_turn, created_at, updated_at) VALUES ('0', NOW(), NOW());
+INSERT INTO boards (player_turn, created_at, updated_at) VALUES ('0', NOW(), NOW());
 INSERT INTO rooms (board_id, status, created_at, updated_at) VALUES (1, 0, NOW(), NOW());
 INSERT INTO rooms (board_id, status, created_at, updated_at) VALUES (2, 0, NOW(), NOW());
 INSERT INTO tiles (board_id, game_tile, created_at, updated_at) VALUES (1, 1, NOW(), NOW());
